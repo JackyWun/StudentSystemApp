e2635d38c2a25c202acf2f6dc28d6d7597a0bc23
JackyWun <wuncheukkiu@gmail.com>
2025-05-14 11:02:34 +0000

    Initial commit for admin app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'pages/login_page.dart'; // <-- ✅ Import your new login page
+import 'pages/login_page.dart';
 
 
 
